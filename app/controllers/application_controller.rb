51a1bdc4be8038697a94e43ddb21c257d9de4381
anhkieu <anhkt1603@gmail.com>
2016-09-24 13:55:17 +0700

    Some files were changed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-  	render :html "Hello, Viet Nam"
+  	render html: "Hello, Viet Nam"
   end
+
 end
